0da308dc467286e2298bc02fc09dca8106b696fc
Kainy Zafari <kainy.zafari@student.ehb.be>
2026-02-04 19:09:20 +0100

    Add SDL3 as a submodule and integrate it into the build system

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,43 @@
 #include <iostream>
+#include <SDL3/SDL.h>
+
 
 int main() {
-    std::cout << "Hello GameEngine v0.1" << std::endl;
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+        return -1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow("Mijn Eerste Engine", 800, 600, SDL_WINDOW_RESIZABLE);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
+    if (!renderer) {
+        std::cerr << "Renderer could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+    }
+
+    if (!window) {
+        std::cerr << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return -1;
+    }
+
+    bool isRunning = true;
+    SDL_Event event;
+
+    while (isRunning) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) {
+                isRunning = false;
+            }
+        }
+        SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+        SDL_RenderClear(renderer);
+
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
